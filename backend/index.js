997deaa14792634c7a45cc8ecbe58e808092fe62
Dan Song <dan.w.song@gmail.com>
2021-05-08 22:03:18 -0400

    Add backend + build scripts

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = 80;
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`app listening at http://localhost:${PORT}`);
+});
